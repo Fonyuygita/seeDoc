8a41763ea24a702f71e22781057ca0b16d8c5951
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-07-08 02:16:54 -0700

    building the ClientForm in component folder, about to add a new custom form, component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
+import ClientForm from "@/components/forms/ClientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-white">
-      <h1>Hello World</h1>
-      <Button>click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <h2 className="text-2xl text-white">Patient Form here</h2>
+          <ClientForm />
+          <div className="text-l4 regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              @ 2024 SeedInnovate
+            </p>
+            <Link href="/?admin=true" className="text-green-500">Admin?</Link>
+          </div>
+
+        </div>
+      </section>
+
+      {/* right side section */}
+      <Image
+        src="/assets/images/doc5.png"
+        height={1000}
+        width={1000}
+        alt="doctor_picture"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
